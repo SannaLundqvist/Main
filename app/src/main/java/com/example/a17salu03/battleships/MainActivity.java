1609a8e004ca6fd1ea45cd8ff31ec0f7bedf7462
MeHasDagger <39961892+MeHasDagger@users.noreply.github.com>
2018-12-05 16:01:15 +0100

    grids

--- app/src/main/java/com/example/a17salu03/battleships/MainActivity.java
+++ app/src/main/java/com/example/a17salu03/battleships/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.a17salu03.battleships;
 
+import android.graphics.Bitmap;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.GridView;
 import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
