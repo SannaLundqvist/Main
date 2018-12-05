8a345d1490f6ce40a54a43140874a3d9c103d105
SannaLundqvist <lundqvist174@gmail.com>
2018-12-05 11:29:59 +0100

    Error fix

--- app/src/main/java/com/example/a17salu03/battleships/MainActivity.java
+++ app/src/main/java/com/example/a17salu03/battleships/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.a17salu03.battleships;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         getActionBar().setTitle("Hola señoita");
-        test
+        TextView text;
      }
 }
