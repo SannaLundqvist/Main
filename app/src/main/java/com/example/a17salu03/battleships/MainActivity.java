179ee394a3e86e5062cee352dbab142ab22f3468
SannaLundqvist <lundqvist174@gmail.com>
2018-12-05 10:53:39 +0100

    ErrorMaking
    eror

--- app/src/main/java/com/example/a17salu03/battleships/MainActivity.java
+++ app/src/main/java/com/example/a17salu03/battleships/MainActivity.java
@@ -9,6 +9,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        getActionBar().setTitle("Hola señorita");
+        getActionBar().setTitle("Hola señoita");
+        ((hej) getActionBar().getDisplayOptions());
     }
 }
