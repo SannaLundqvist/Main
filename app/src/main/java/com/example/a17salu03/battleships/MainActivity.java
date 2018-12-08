e9f30294e3a59499f2843cab2e98d47f96888513
MeHasDagger <39961892+MeHasDagger@users.noreply.github.com>
2018-12-08 14:30:47 +0100

    Början på Grid

--- app/src/main/java/com/example/a17salu03/battleships/MainActivity.java
+++ app/src/main/java/com/example/a17salu03/battleships/MainActivity.java
@@ -1,12 +1,11 @@
 package com.example.a17salu03.battleships;
 
-import android.graphics.Bitmap;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.GridView;
-import android.widget.TextView;
+import android.view.View;
+import android.widget.Button;
 
-import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,6 +14,69 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Button startButton = (Button) findViewById(R.id.quick_game);
+  //      Button aboutButton = (Button) findViewById(R.id.aboutButton);
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, GridActivity.class);
+                startActivity(intent);
+            }
+        });
 
-     }
+
+ }
 }
+
+/*
+public class MainActivity extends AppCompatActivity {
+
+    private BoardView boardView;
+    private GameEngine gameEngine;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        boardView = (BoardView) findViewById(board);
+        gameEngine = new GameEngine();
+        boardView.setGameEngine(gameEngine);
+        boardView.setMainActivity(this);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.action_new_game) {
+            newGame();
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void gameEnded(char c) {
+        String msg = (c == 'T') ? "Game Ended. Tie" : "GameEnded. " + c + " win";
+
+        new AlertDialog.Builder(this).setTitle("Tic Tac Toe").
+                setMessage(msg).
+                setOnDismissListener(new DialogInterface.OnDismissListener() {
+                    @Override
+                    public void onDismiss(DialogInterface dialogInterface) {
+                        newGame();
+                    }
+                }).show();
+    }
+
+    private void newGame() {
+        gameEngine.newGame();
+        boardView.invalidate();
+    }
+
+
+}
+*/
