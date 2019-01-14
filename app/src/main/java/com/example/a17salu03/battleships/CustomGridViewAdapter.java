c7053aa9bcf81816012743f4293a8727ca4b9973
MeHasDagger <39961892+MeHasDagger@users.noreply.github.com>
2019-01-14 22:25:14 +0100

    Update CustomGridViewAdapter.java

--- app/src/main/java/com/example/a17salu03/battleships/CustomGridViewAdapter.java
+++ app/src/main/java/com/example/a17salu03/battleships/CustomGridViewAdapter.java
@@ -1,5 +1,6 @@
 package com.example.a17salu03.battleships;
 
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
@@ -14,7 +15,7 @@ public class CustomGridViewAdapter extends BaseAdapter {
     private boolean isClickable;
     private ArrayList<Tile> tiles;
     private View view;
-    private  ImageView mImageView;
+    private ImageView mImageView;
     private int rotateDegrees;
 
     public static final String TILE_TYPE_WATER = "W";
@@ -95,9 +96,9 @@ public class CustomGridViewAdapter extends BaseAdapter {
         return mImageView;
     }
 
-    private void setImage(int position){
+    private void setImage(int position) {
         if (isFriendlyBoard) {
-            switch (boardState[position]){
+            switch (boardState[position]) {
                 case TILE_TYPE_WATER:
                     mImageView.setImageResource(R.drawable.water_tile);
                     break;
@@ -207,8 +208,8 @@ public class CustomGridViewAdapter extends BaseAdapter {
                     mImageView.setImageBitmap(BitMapEdit.combineImages(R.drawable.skepp_3r_w_r_broken, view, 90, false));
                     break;
             }
-        } else if (!isFriendlyBoard){
-            switch (boardState[position]){
+        } else if (!isFriendlyBoard) {
+            switch (boardState[position]) {
                 case TILE_TYPE_WATER:
                     mImageView.setImageResource(R.drawable.water_tile);
                     break;
@@ -219,106 +220,148 @@ public class CustomGridViewAdapter extends BaseAdapter {
                     mImageView.setImageResource(R.drawable.water_tile);
                     break;
                 case TILE_TYPE_SIZE_1_SHIPID_1_DAMAGED:
-                    if (isShipDestroyed(boardState[position], position)){
-
-                    }
-                    mImageView.setImageBitmap(BitMapEdit.combineImages(R.drawable.skepp_1r_w_broken, view, 0, false));
+                    if (isShipDestroyed(boardState[position], position))
+                        mImageView.setImageBitmap(BitMapEdit.combineImages(R.drawable.skepp_1r_w_broken, view, 0, false));
                     break;
                 case TILE_TYPE_SIZE_1_SHIPID_2:
                     mImageView.setImageResource(R.drawable.water_tile);
                     break;
                 case TILE_TYPE_SIZE_1_SHIPID_2_DAMAGED:
-                    mImageView.setImageBitmap(BitMapEdit.combineImages(R.drawable.skepp_1r_w_broken, view, 0, false));
+                    if (isShipDestroyed(boardState[position], position))
+                        mImageView.setImageBitmap(BitMapEdit.combineImages(R.drawable.skepp_1r_w_broken, view, 0, false));
                     break;
                 case TILE_TYPE_SIZE_1_SHIPID_3:
                     mImageView.setImageResource(R.drawable.water_tile);
                     break;
                 case TILE_TYPE_SIZE_1_SHIPID_3_DAMAGED:
-                    mImageView.setImageBitmap(BitMapEdit.combineImages(R.drawable.skepp_1r_w_broken, view, 0, false));
+                    if (isShipDestroyed(boardState[position], position))
+                        mImageView.setImageBitmap(BitMapEdit.combineImages(R.drawable.skepp_1r_w_broken, view, 0, false));
                     break;
                 case TILE_TYPE_SIZE_2_SHIPID_4_H_L:
                     mImageView.setImageResource(R.drawable.water_tile);
                     break;
                 case TILE_TYPE_SIZE_2_SHIPID_4_H_L_DAMAGED:
-                    mImageView.setImageBitmap(BitMapEdit.combineImages(R.drawable.skepp_2r_w_l_broken, view, 0, false));
+                    if (isShipDestroyed(boardState[position], position))
+                        mImageView.setImageBitmap(BitMapEdit.combineImages(R.drawable.skepp_2r_w_l_broken, view, 0, false));
+                    else
+                        mImageView.setImageBitmap(BitMapEdit.combineImages(R.drawable.broken_parts, view, 360, false));
                     break;
                 case TILE_TYPE_SIZE_2_SHIPID_4_H_R:
                     mImageView.setImageResource(R.drawable.water_tile);
                     break;
                 case TILE_TYPE_SIZE_2_SHIPID_4_H_R_DAMAGED:
-                    mImageView.setImageBitmap(BitMapEdit.combineImages(R.drawable.skepp_2r_w_r_broken, view, 0, false));
+                    if (isShipDestroyed(boardState[position], position))
+                        mImageView.setImageBitmap(BitMapEdit.combineImages(R.drawable.skepp_2r_w_r_broken, view, 0, false));
+                    else
+                        mImageView.setImageBitmap(BitMapEdit.combineImages(R.drawable.broken_parts, view, 360, false));
                     break;
                 case TILE_TYPE_SIZE_2_SHIPID_4_V_L:
                     mImageView.setImageResource(R.drawable.water_tile);
                     break;
                 case TILE_TYPE_SIZE_2_SHIPID_4_V_L_DAMAGED:
-                    mImageView.setImageBitmap(BitMapEdit.combineImages(R.drawable.skepp_2r_w_l_broken, view, 90, true));
+                    if (isShipDestroyed(boardState[position], position))
+                        mImageView.setImageBitmap(BitMapEdit.combineImages(R.drawable.skepp_2r_w_l_broken, view, 90, true));
+                    else
+                        mImageView.setImageBitmap(BitMapEdit.combineImages(R.drawable.broken_parts, view, 360, false));
                     break;
                 case TILE_TYPE_SIZE_2_SHIPID_4_V_R:
                     mImageView.setImageResource(R.drawable.water_tile);
                     break;
                 case TILE_TYPE_SIZE_2_SHIPID_4_V_R_DAMAGED:
-                    mImageView.setImageBitmap(BitMapEdit.combineImages(R.drawable.skepp_2r_w_r_broken, view, 90, true));
+                    if (isShipDestroyed(boardState[position], position))
+                        mImageView.setImageBitmap(BitMapEdit.combineImages(R.drawable.skepp_2r_w_r_broken, view, 90, true));
+                    else
+                        mImageView.setImageBitmap(BitMapEdit.combineImages(R.drawable.broken_parts, view, 360, false));
                     break;
                 case TILE_TYPE_SIZE_2_SHIPID_5_H_L:
                     mImageView.setImageResource(R.drawable.water_tile);
                     break;
                 case TILE_TYPE_SIZE_2_SHIPID_5_H_L_DAMAGED:
-                    mImageView.setImageBitmap(BitMapEdit.combineImages(R.drawable.skepp_2r_w_l_broken, view, 0, false));
+                    if (isShipDestroyed(boardState[position], position))
+                        mImageView.setImageBitmap(BitMapEdit.combineImages(R.drawable.skepp_2r_w_l_broken, view, 0, false));
+                    else
+                        mImageView.setImageBitmap(BitMapEdit.combineImages(R.drawable.broken_parts, view, 360, false));
                     break;
                 case TILE_TYPE_SIZE_2_SHIPID_5_H_R:
                     mImageView.setImageResource(R.drawable.water_tile);
                     break;
                 case TILE_TYPE_SIZE_2_SHIPID_5_H_R_DAMAGED:
-                    mImageView.setImageBitmap(BitMapEdit.combineImages(R.drawable.skepp_2r_w_r_broken, view, 0, false));
+                    if (isShipDestroyed(boardState[position], position))
+                        mImageView.setImageBitmap(BitMapEdit.combineImages(R.drawable.skepp_2r_w_r_broken, view, 0, false));
+                    else
+                        mImageView.setImageBitmap(BitMapEdit.combineImages(R.drawable.broken_parts, view, 360, false));
                     break;
                 case TILE_TYPE_SIZE_2_SHIPID_5_V_L:
                     mImageView.setImageResource(R.drawable.water_tile);
                     break;
                 case TILE_TYPE_SIZE_2_SHIPID_5_V_L_DAMAGED:
-                    mImageView.setImageBitmap(BitMapEdit.combineImages(R.drawable.skepp_2r_w_l_broken, view, 90, true));
+                    if (isShipDestroyed(boardState[position], position))
+                        mImageView.setImageBitmap(BitMapEdit.combineImages(R.drawable.skepp_2r_w_l_broken, view, 0, false));
+                    else
+                        mImageView.setImageBitmap(BitMapEdit.combineImages(R.drawable.broken_parts, view, 360, false));
                     break;
                 case TILE_TYPE_SIZE_2_SHIPID_5_V_R:
                     mImageView.setImageResource(R.drawable.water_tile);
                     break;
                 case TILE_TYPE_SIZE_2_SHIPID_5_V_R_DAMAGED:
-                    mImageView.setImageBitmap(BitMapEdit.combineImages(R.drawable.skepp_2r_w_r_broken, view, 90, true));
+                    if (isShipDestroyed(boardState[position], position))
+                        mImageView.setImageBitmap(BitMapEdit.combineImages(R.drawable.skepp_2r_w_r_broken, view, 90, true));
+                    else
+                        mImageView.setImageBitmap(BitMapEdit.combineImages(R.drawable.broken_parts, view, 360, false));
                     break;
                 case TILE_TYPE_SIZE_3_SHIPID_6_H_L:
                     mImageView.setImageResource(R.drawable.water_tile);
                     break;
                 case TILE_TYPE_SIZE_3_SHIPID_6_H_L_DAMAGED:
-                    mImageView.setImageBitmap(BitMapEdit.combineImages(R.drawable.skepp_3r_w_l_broken, view, 0, false));
+                    if (isShipDestroyed(boardState[position], position))
+                        mImageView.setImageBitmap(BitMapEdit.combineImages(R.drawable.skepp_3r_w_l_broken, view, 0, false));
+                    else
+                        mImageView.setImageBitmap(BitMapEdit.combineImages(R.drawable.broken_parts, view, 360, false));
                     break;
                 case TILE_TYPE_SIZE_3_SHIPID_6_H_M:
                     mImageView.setImageResource(R.drawable.water_tile);
                     break;
                 case TILE_TYPE_SIZE_3_SHIPID_6_H_M_DAMAGED:
-                    mImageView.setImageBitmap(BitMapEdit.combineImages(R.drawable.skepp_3r_w_m_broken, view, 0, false));
+                    if (isShipDestroyed(boardState[position], position))
+                        mImageView.setImageBitmap(BitMapEdit.combineImages(R.drawable.skepp_3r_w_m_broken, view, 0, false));
+                    else
+                        mImageView.setImageBitmap(BitMapEdit.combineImages(R.drawable.broken_parts, view, 360, false));
                     break;
                 case TILE_TYPE_SIZE_3_SHIPID_6_H_R:
                     mImageView.setImageResource(R.drawable.water_tile);
                     break;
                 case TILE_TYPE_SIZE_3_SHIPID_6_H_R_DAMAGED:
-                    mImageView.setImageBitmap(BitMapEdit.combineImages(R.drawable.skepp_3r_w_r_broken, view, 0, false));
+                    if (isShipDestroyed(boardState[position], position))
+                        mImageView.setImageBitmap(BitMapEdit.combineImages(R.drawable.skepp_3r_w_r_broken, view, 0, false));
+                    else
+                        mImageView.setImageBitmap(BitMapEdit.combineImages(R.drawable.broken_parts, view, 360, false));
                     break;
                 case TILE_TYPE_SIZE_3_SHIPID_6_V_L:
                     mImageView.setImageResource(R.drawable.water_tile);
                     break;
                 case TILE_TYPE_SIZE_3_SHIPID_6_V_L_DAMAGED:
-                    mImageView.setImageBitmap(BitMapEdit.combineImages(R.drawable.skepp_3r_w_l_broken, view, 90, false));
+                    if (isShipDestroyed(boardState[position], position))
+                        mImageView.setImageBitmap(BitMapEdit.combineImages(R.drawable.skepp_3r_w_l_broken, view, 90, false));
+                    else
+                        mImageView.setImageBitmap(BitMapEdit.combineImages(R.drawable.broken_parts, view, 360, false));
                     break;
                 case TILE_TYPE_SIZE_3_SHIPID_6_V_M:
                     mImageView.setImageResource(R.drawable.water_tile);
                     break;
                 case TILE_TYPE_SIZE_3_SHIPID_6_V_M_DAMAGED:
-                    mImageView.setImageBitmap(BitMapEdit.combineImages(R.drawable.skepp_3r_w_m_broken, view, 90, false));
+                    if (isShipDestroyed(boardState[position], position))
+                        mImageView.setImageBitmap(BitMapEdit.combineImages(R.drawable.skepp_3r_w_m_broken, view, 90, false));
+                    else
+                        mImageView.setImageBitmap(BitMapEdit.combineImages(R.drawable.broken_parts, view, 360, false));
                     break;
                 case TILE_TYPE_SIZE_3_SHIPID_6_V_R:
                     mImageView.setImageResource(R.drawable.water_tile);
                     break;
                 case TILE_TYPE_SIZE_3_SHIPID_6_V_R_DAMAGED:
-                    mImageView.setImageBitmap(BitMapEdit.combineImages(R.drawable.skepp_3r_w_r_broken, view, 90, false));
+                    if (isShipDestroyed(boardState[position], position))
+                        mImageView.setImageBitmap(BitMapEdit.combineImages(R.drawable.skepp_3r_w_r_broken, view, 90, false));
+                    else
+                        mImageView.setImageBitmap(BitMapEdit.combineImages(R.drawable.broken_parts, view, 360, false));
                     break;
             }
         }
@@ -327,17 +370,51 @@ public class CustomGridViewAdapter extends BaseAdapter {
 
     private boolean isShipDestroyed(String value, int position) {
         boolean isDestroyed = false;
-        if (value.charAt(0) <= '3'){
+        if (value.charAt(0) <= '3') {
             isDestroyed = true;
-        } else if (value.charAt(0) == '4' && value.charAt(0) == '5'){
-            if (value.charAt(0) == boardState[position + 1].charAt(0) || value.charAt(0) == boardState[position - 1].charAt(0) || value.charAt(0) == boardState[position + 7].charAt(0) || value.charAt(0) == boardState[position - 7].charAt(0)){
-
+        } else if (value.charAt(0) == '4' && value.charAt(0) == '5') {
+            try {
+                if (value.charAt(0) == boardState[position + 1].charAt(0) || value.charAt(0) == boardState[position - 1].charAt(0) || value.charAt(0) == boardState[position + 7].charAt(0) || value.charAt(0) == boardState[position - 7].charAt(0))
+                    isDestroyed = true;
+            } catch (ArrayIndexOutOfBoundsException e) {
+                Log.d("CustomGridViewAdapter", "ArrayOutOfBounds");
             }
-        } else if (value.charAt(0) == '6'){
+        } else if (value.charAt(0) == '6') {
+            try {
+                String matchingPosition = null;
+                if (value.charAt(0) == boardState[position + 1].charAt(0))
+                    matchingPosition = "east";
+                else if (value.charAt(0) == boardState[position - 1].charAt(0))
+                    matchingPosition = "west";
+                else if (value.charAt(0) == boardState[position + 7].charAt(0))
+                    matchingPosition = "south";
+                else if (value.charAt(0) == boardState[position - 7].charAt(0))
+                    matchingPosition = "north";
 
+                if (matchingPosition != null) {
+                    switch (matchingPosition) {
+                        case "east":
+                            if (value.charAt(0) == boardState[position + 2].charAt(0))
+                                isDestroyed = true;
+                            break;
+                        case "west":
+                            if (value.charAt(0) == boardState[position - 2].charAt(0))
+                                isDestroyed = true;
+                            break;
+                        case "south":
+                            if (value.charAt(0) == boardState[position + 14].charAt(0))
+                                isDestroyed = true;
+                            break;
+                        case "north":
+                            if (value.charAt(0) == boardState[position - 14].charAt(0))
+                                isDestroyed = true;
+                            break;
+                    }
+                }
+            } catch (ArrayIndexOutOfBoundsException e) {
+                Log.d("CustomGridViewAdapter", "ArrayOutOfBounds");
+            }
         }
         return isDestroyed;
     }
-
-
 }
